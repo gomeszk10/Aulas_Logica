a5bf477bb594a50e6edc35bae2ec1eeebfce4f8a
Guilherme Gomes Olivatto <116249577+gomeszk10@users.noreply.github.com>
2023-03-27 22:09:37 -0300

    Delete Logica_Programacao/Logica_Programacao directory

--- Logica_Programacao/Logica_Programacao/Aula_02/main.js
+++
@@ -1,5 +0,0 @@
-var nome = "Joãozinho"
-var altura = 1.74
-var peso = 60
-var imc = peso / (altura * altura)
-console.log ("Seu nome é: " + nome + " Sua altura é: " + altura + " Seu peso é: " + peso + " Seu IMC é: " + imc )
--- Logica_Programacao/Logica_Programacao/Aula_03/js/script2.js
+++
@@ -1,6 +0,0 @@
-var peso = parseFloat(prompt("Digite o seu peso: "));
-var altura = parseFloat(prompt("Digite sua altura: "));
-
-var imc = peso / (altura^2);
-document.write(imc);
-
