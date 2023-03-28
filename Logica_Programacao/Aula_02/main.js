14b35d08fc76f94be480a7519ec9e2e393ce5e72
Guilherme Gomes Olivatto <116249577+gomeszk10@users.noreply.github.com>
2023-03-27 21:15:03 -0300

    Add arquivos das Primeiras Aulas de Logica

---
+++ Logica_Programacao/Aula_02/main.js
@@ -0,0 +1,5 @@
+var nome = "Joãozinho"
+var altura = 1.74
+var peso = 60
+var imc = peso / (altura * altura)
+console.log ("Seu nome é: " + nome + " Sua altura é: " + altura + " Seu peso é: " + peso + " Seu IMC é: " + imc )
