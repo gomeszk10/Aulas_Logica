9943faa6d6217fb0ec3673a416696b2c442cfd6e
Guilherme Gomes Olivatto <116249577+gomeszk10@users.noreply.github.com>
2023-03-27 21:31:50 -0300

    add novas modificações

---
+++ Logica_Programacao/Aula_03/js/script.js
@@ -0,0 +1,12 @@
+// declaração de variável
+// tipo String
+var aluno = "Jaqueline Sensação"
+// tipo number/int
+var nota1 = 7
+var nota2 = 10
+var nota3 = 6
+var nota4 = 2
+var nota5 = 10
+var media = (nota1+nota2+nota3+nota4+nota5) /5
+
+console.log ("O aluno" + aluno + "Obteve a média: " + media)
