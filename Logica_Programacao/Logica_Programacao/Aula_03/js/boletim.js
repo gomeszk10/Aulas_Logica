38d68ddac90350ce244758206197e8df24ccc1e5
Guilherme Gomes Olivatto <116249577+gomeszk10@users.noreply.github.com>
2023-03-27 22:08:32 -0300

    Atualizações aula

---
+++ Logica_Programacao/Logica_Programacao/Aula_03/js/boletim.js
@@ -0,0 +1,14 @@
+// declaração de variável
+// tipo String
+// promt é uma janelinha que aparece na tela
+// parseFloat é transformar um número quebrado em um número inteiro 
+var aluno = "Jaqueline Sensação"
+// tipo number/int
+var nota1 = 7
+var nota2 = 10
+var nota3 = 6
+var nota4 = 2
+var nota5 = 10
+var media = (nota1+nota2+nota3+nota4+nota5) /5
+
+console.log ("O aluno" + aluno + "Obteve a média: " + media)
---
+++ Logica_Programacao/Logica_Programacao/Aula_03/js/script2.js
@@ -0,0 +1,6 @@
+var peso = parseFloat(prompt("Digite o seu peso: "));
+var altura = parseFloat(prompt("Digite sua altura: "));
+
+var imc = peso / (altura^2);
+document.write(imc);
+
